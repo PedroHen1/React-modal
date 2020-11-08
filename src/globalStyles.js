fffee821c90c4ee72efc976505b16afd7f19c7fe
PedroHen1 <pedroex6tenz@gmail.com>
2020-11-08 11:50:28 -0300

    initial commit

---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    padding: 0;
+    marging: 0;
+}
+`
